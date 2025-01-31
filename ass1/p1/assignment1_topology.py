e68e1df14b495036b66df254229c6b65d29ef575
tch0905 <a1182116615@gmail.com>
2025-02-01 03:25:19 +0800

    py test

--- ass1/p1/assignment1_topology.py
+++ ass1/p1/assignment1_topology.py
@@ -55,10 +55,13 @@ if __name__ == '__main__':
     net.start()
     h1 = net.get('h1')
     h2 = net.get('h2')
-    h1.cmd('iperf -s &')
+    iperf_server_output = h1.cmd('iperf -s &')
     iperf_client_output = h2.cmd('iperf -c h1')
 
-    # Print the results of the iperf client
+    print("iPerf server Output:")
+    print(iperf_server_output)
+
+
     print("iPerf Client Output:")
     print(iperf_client_output)
     CLI( net )
