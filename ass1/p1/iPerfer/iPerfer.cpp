5b30fa9edf8c749bf75b8ecf5961355176a02f39
tch0905 <a1182116615@gmail.com>
2025-02-01 05:55:02 +0800

    time fix

--- ass1/p1/iPerfer/iPerfer.cpp
+++ ass1/p1/iPerfer/iPerfer.cpp
@@ -137,7 +137,7 @@ void run_client(const char* hostname, int port, int time_sec) {
     while (sending) {
         clock_t current_time = clock();
         double elapsed =  (double)(current_time - start_time) / CLOCKS_PER_SEC;
-        if (elapsed >= time_sec) {
+        if (elapsed >=  (double)time_sec) {
             sending = false;
             break;
         }
